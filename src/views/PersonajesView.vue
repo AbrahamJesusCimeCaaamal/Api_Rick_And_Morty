07412efa2111c1549cbca3d39bfe7a0f9d5cf7ef
unknown <abraham.19070019@itsmotul.edu.mx>
2023-02-25 13:58:02 -0600

    ventana de personajes

--- src/views/PersonajesView.vue
+++ src/views/PersonajesView.vue
@@ -8,7 +8,7 @@ import Personajes from "../components/Personajes.vue";
     <div class=" bg-purple-300 px-6  text-center">
     <h1 class=" px-14 text-3xl py-2"> <strong> Personajes del programa</strong> </h1>
 
-    
+    <Personajes />
   </div>
 
   
