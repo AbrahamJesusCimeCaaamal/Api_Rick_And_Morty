d576d9771090aa16ed1059f671a5ace635364e00
unknown <abraham.19070019@itsmotul.edu.mx>
2023-02-25 13:56:17 -0600

    Ventana de inicio modificado

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -9,6 +9,57 @@
     y le mostrará cómo realizar diferentes consultas, para que pueda aprovecharla al máximo
   </p>
 
+  <h1 class="text-2xl py-10 px-4">
+    <strong> ¿Qué trata la serie Rick y Morty?</strong> 
+
+  </h1>
+  <p class="text-xl py-6 px-4">
+    La serie sigue las desventuras de un científico, Rick Sanchez,
+ y su fácilmente influenciable nieto, Morty, quienes pasan el tiempo
+  entre la vida doméstica y los viajes espaciales, temporales e intergalácticos.
+  </p>
+  <div class="grid grid-cols-2 ">
+  <div class=" w-full p-2 text-center bg-blue-600 lg:px-48 lg:py-40 md:px-14 md:py-80  ">
+    <img class=" md:scale-125 bg-black py-1 px-2" src="./Rick_y_morty.png" width="300" height="300" walt=" ">
+
+  </div>
+  <div class=" font-semibold w-full p-2 text-center bg-blue-300 sm:px-2 sm:py-36 md:px-24 md:py-36 lg:px-24 lg:py-14">
+    <h1 class="text-2xl py-2"><strong>Ficha Técnica</strong> </h1>
+<div class=" text-justify">
+  <h1 class="text-base "> <strong> Título:</strong> Rick and Morty</h1>
+    <h1 class="text-base"><strong>Género:</strong> Animación, comedia, aventuras</h1>
+    <h1 class="text-base "><strong> Temporadas:</strong> 3</h1>
+    <h1 class="text-base"> <strong>Episodios:</strong> 31 </h1>
+    <h1 class="text-base"><strong> Fecha de emisión:</strong> 2013</h1>
+    <h1 class="text-base"><strong>Creadores:</strong>Justin Roiland y Dan Harmon </h1>
+    <h1 class="text-base"> <strong>Distribuidora:</strong> Adult Swim, Warner Bros. Televisión </h1>
+ 
+</div><br>
+
+  
+  <h1 class="text-2xl text-green-600"> <strong> Sinopsis</strong> </h1> <br>
+<div class=" text-justify">
+  <p>Rick Sánchez
+es la definición exacta de "científico loco". Es alcohólico, es un
+genio, es irresponsable y está loco. Rick acaba de mudarse a casa de su hija
+Beth y allí recuerda que tiene un nieto llamado Morty. Sin preguntar a nadie,
+decide que va a obligarle a que le acompañe a todo tipo de aventuras para que
+el chico se vuelva inteligente como él y no se convierta en un idiota como
+Jerry, padre de Morty y yerno de Rick. </p>
+<br>
+<p>Así, Rick y
+Morty comienzan a vivir aventuras intergalácticas a pesar de que la familia no
+quiere que lo sigan haciendo. Poco a poco tienen que intentar encontrar un
+equilibrio entre su vida familiar y sus viajes a través del espacio y por
+distintas realidades paralelas, algo que no es fácil para el pequeño Morty que
+es incapaz de tener una vida normal al margen de su abuelo.</p>
+
+</div>
+  
+
+  </div>
+
+</div>
  
   </div>
 
